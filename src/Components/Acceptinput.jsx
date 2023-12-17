749356c2fee9dfc990c18e1f1180bb84c9292285
ANISH BISWAS <iammanishbiswas@gmail.com>
2023-12-17 22:29:22 +0530

    D

--- src/Components/Acceptinput.jsx
+++ src/Components/Acceptinput.jsx
@@ -19,12 +19,12 @@ function Acceptinput(props) {
       alert('You need to enter a group name to proceed');
       b = false;
     }
-    
+
     let initials;
     let d = true;
 
     if (w.length == 1) {
-     initials = g.substring(0,1) + " ";
+      initials = g.substring(0, 1) + " ";
       d = false;
     }
 
@@ -35,25 +35,25 @@ function Acceptinput(props) {
 
     if (b == true) {
 
-      if (d == true){
-      const firstCharSecondWord = w[1].charAt(0);
-      console.log(firstCharSecondWord); 
-      initials = g.substring(0, 1) + firstCharSecondWord;
-      console.log(initials);
-    }
+      if (d == true) {
+        const firstCharSecondWord = w[1].charAt(0);
+        console.log(firstCharSecondWord);
+        initials = g.substring(0, 1) + firstCharSecondWord;
+        console.log(initials);
+      }
 
-    let a = JSON.parse(localStorage.getItem("storename"));
-    console.log(a);
-    console.log(typeof(a));
-    for (let i = 0; i < a.length; i++) {
-      let name = a[i];
-      if (name == g) {
+      let a = JSON.parse(localStorage.getItem("storename"));
+      console.log(a);
+      console.log(typeof (a));
+      for (let i = 0; i < a.length; i++) {
+        let name = a[i];
+        if (name == g) {
           alert('This group name is already present , please try another group name!');
           return;
+        }
       }
-  }
-  a.push(g);
-  localStorage.setItem("storename",JSON.stringify(a));
+      a.push(g);
+      localStorage.setItem("storename", JSON.stringify(a));
 
       console.log(initials);
       let notes = [];
@@ -64,10 +64,10 @@ function Acceptinput(props) {
       let c = localStorage.getItem('index');
       let b = JSON.parse(c);
       b = b.map((element) => Number(element) + 1);
-      if (b[0]<10)
-      b[0] = "0" + b[0];
+      if (b[0] < 10)
+        b[0] = "0" + b[0];
       else
-      b[0] = String(b[0]);
+        b[0] = String(b[0]);
       g = b[0] + color + initials.toUpperCase() + g;
       localStorage.setItem(b[0], notesapp);
       localStorage.setItem('index', JSON.stringify(b));
@@ -78,7 +78,7 @@ function Acceptinput(props) {
       const interests = localStorage.getItem("Gn");
       console.log(interests);
       const data = JSON.parse(interests);
-      console.log(typeof(data));
+      console.log(typeof (data));
       data.push(g);
       let demo = JSON.stringify(data);
       localStorage.setItem("Gn", demo);
@@ -118,26 +118,26 @@ function Acceptinput(props) {
 
   return (props.trigger) ? (
     <div className={styles.popupcontainer}>
-    <div className={styles.popup}>
+      <div className={styles.popup}>
 
-      <div className={styles.title}>Create New group</div><br />
-      <div className={styles.gn}>Group Name</div><br />
-      <div className={styles.cc}>Choose colour</div><br />
-      <input type='text' className={styles.text} placeholder='Enter group name' id='groupname' />
-      <div className={styles.violet} onClick={() => { setcolor('#B38BFA') }} />
-      <div className={styles.violet} style={{ position: 'absolute', left: '26vw', background: '#FF79F2' }}
-        onClick={() => { setcolor('#FF79F2') }} />
-      <div className={styles.violet} style={{ position: 'absolute', left: '30vw', background: '#43E6FC' }}
-        onClick={() => { setcolor('#43E6FC') }} />
-      <div className={styles.violet} style={{ position: 'absolute', left: '34vw', background: '#F19576' }}
-        onClick={() => { setcolor('#F19576') }} />
-      <div className={styles.violet} style={{ position: 'absolute', left: '38vw', background: '#0047FF' }}
-        onClick={() => { setcolor('#0047FF') }} />
-      <div className={styles.violet} style={{ position: 'absolute', left: '42vw', background: '#6691FF' }}
-        onClick={() => { setcolor('#6691FF') }} />
-      <div className={styles.crtbtn} onClick={() => create()}><p style={{ marginTop: '2px' , cursor:'pointer' }}>
-        Create</p></div>
-    </div>
+        <div className={styles.title}>Create New group</div><br />
+        <div className={styles.gn}>Group Name</div><br />
+        <div className={styles.cc}>Choose colour</div><br />
+        <input type='text' className={styles.text} placeholder='Enter group name' id='groupname' />
+        <div className={styles.violet} onClick={() => { setcolor('#B38BFA') }} />
+        <div className={styles.violet} style={{ position: 'absolute', left: '26vw', background: '#FF79F2' }}
+          onClick={() => { setcolor('#FF79F2') }} />
+        <div className={styles.violet} style={{ position: 'absolute', left: '30vw', background: '#43E6FC' }}
+          onClick={() => { setcolor('#43E6FC') }} />
+        <div className={styles.violet} style={{ position: 'absolute', left: '34vw', background: '#F19576' }}
+          onClick={() => { setcolor('#F19576') }} />
+        <div className={styles.violet} style={{ position: 'absolute', left: '38vw', background: '#0047FF' }}
+          onClick={() => { setcolor('#0047FF') }} />
+        <div className={styles.violet} style={{ position: 'absolute', left: '42vw', background: '#6691FF' }}
+          onClick={() => { setcolor('#6691FF') }} />
+        <div className={styles.crtbtn} onClick={() => create()}><p style={{ marginTop: '2px', cursor: 'pointer' }}>
+          Create</p></div>
+      </div>
     </div>
   ) : "";
 }
